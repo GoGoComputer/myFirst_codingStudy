53a5ac52c01aa439095207fa508fae3746b06608
GoGoComputer <vividsaebom@gmail.com>
2024-01-12 12:52:15 +0900

    [update] function

---
+++ javascriptStudy/3_function.js
@@ -0,0 +1,41 @@
+// 함수
+
+//f(x) = 2x + 3
+
+    // x =1 INPUT x 들ㅇ어오는 값
+            
+                //------------------
+                //    FUNCTION f : |
+                //------------------
+
+                            // OUTPUT (x)
+
+// (수학) == (프로그래밍)
+
+
+
+// f(x) = 2x + 3
+
+    // 정의 
+    function myFunction (x) {
+        let temp = 2 * x + 3;
+        return temp; // outuput 값
+
+    }
+
+    // 실행
+    myFunction(1); // 5
+
+    // 변수 이름은 맥락있게 지어주는 것이 좋다.
+
+
+
+// f (x, y) = x + y
+
+// 정의
+// function add (x, y) {
+//     let temp = x + y;
+//     return temp;
+
+// 실행
+// add(1, 2); 
